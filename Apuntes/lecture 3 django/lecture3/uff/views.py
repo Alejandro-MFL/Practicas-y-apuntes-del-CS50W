c8fb37fedd1a06e42ac1f7bae26eca03031767dc
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-08-15 17:52:08 +0200

    Commit: 15-08-2025_17:52:08

--- Apuntes/lecture 3 django/lecture3/uff/views.py
+++ Apuntes/lecture 3 django/lecture3/uff/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render( request , "uffs/index.html", {"name": "Alex"                  
+    })
+def pepe(request, name):
+    return render( request , "uffs/index.html", {"name": name.capitalize()                  
+    })
