a0bfd3457832c42814fadc170eff6b466a5d84fc
AlejandroMFL <fernandezlucas.am@gmail.com>
2025-09-14 13:43:29 +0200

    Clase 2 de CSS añadida

--- Apuntes/lecture 0 html y css/CSS.py
+++ Apuntes/lecture 0 html y css/CSS.py
@@ -1,20 +1,53 @@
 #Estos apuntes no pertenecen al CS50
 
 # Basico 
-Basico=""" 
-    Color: -No usar rgba, rgb()
+Bordes=""" 
+    De serie afecta al tamaño del objeto, como el padding, para poder evitarlo puedes cambiar "box-sizing"
+        box-sizing: content-box     # Aumenta el tamaño basico
+        box-sizing: border-box      # Fuerza a que el tamaño total englobe el borde
 
-    Texto:  - Si usas fonts que tiene el usuario no las cargas
-            - Puedes usar y no solo en texto elementos supletorios 
-            ###
-                font-family: system-ui, Ubuntu, sans-serif  #Se usan de izquierda a derecha
-            ###
-                
-    Categorias: bloques(cajas) y lineal(letras)
-
-    Bordes:  - Es un elemento de "objeto, aumentarlo aumenta el tamaño
-                outline: se puede usar para no tocar el borde
-                box-sizing: content-box de base o con border-box
+    outline: Otra opcion es usar "outline" es un borde dentro del objeto, antes del borde y no afecta al tamaño del objeto                 
+"""
+Desbordamiento= """ 
+#Cuando el contenido es mas grande que el continente se desborda
+    Soluciones:
+        -overflow: x; x = ...
+            visible         # Desborda
+            hidden          # Corta el contenido sobrante
+            scroll          # Corta el contenido pero siempre agrega barras para desplazarte por el contenido, aunque no desborde
+            auto            # Dependiendo del navegador y si hay desbordamiento usa un metodo para navegar por el contenido(normalmente barras)
+       
+        -text-overflow: x; x = ...
+            ellipsis        # Corta el texto y añade puntos suspensivos
+            clip            # El predeterminado
+"""
+Posicionamiento=""" 
+# El posicionamiento de serie es estatico
+    position: x; x = ...
+        absolute        # Fuerza su espacio de forma absoluta respecto al primer elemento de referencia
+        relative        # Crea un elemento de referencia para los hijos
+        fixed           # Da una posicion fija respecto a la pantalla
+        sticky          # hace que se desplace por el contenedor
+"""
+Profundidad=""" 
+# Se usa un sistema de capas para dar profundidad, estas se puedes tocar a traves de:
+    z-indez: y;     y = cualquier numero
+"""
+BloquesTipo=""" 
+    Categorias: bloques(cajas) y lineal(letras). Se pueden alterar por
+        -display: x; x= ...
+            inline      
+            block
+            flex    # Esto afecta a como almacena a sus descendientes, forma flexible
+            grid    # Esto afecta a como almacena a sus descendientes, forma cuadricula
+    
+"""
+Texto= """ 
+    - Si usas fonts que tiene el usuario no las cargas
+    - Puedes usar elementos supletorios 
+    ###
+        font-family: system-ui, Ubuntu, sans-serif  #Se usan de izquierda a derecha
+    ###
 """
 Seudoclases= """ 
 # Sirven para seleccionar elementos en un estado concreto (:y)
@@ -57,7 +90,6 @@ Prioridad = """
         4. type  
 
 """
-
 ResponsiveDesign=""" 
 #Hace referencia a la escabilidad para diferentes formatos.
     Se puede añadir que el ancho cambie automaticamente con el ancho de la pagina
@@ -70,9 +102,50 @@ ResponsiveDesign="""
 
 """
 
+# Fexbox
+DireccionFlex=""" 
+    Se puede organizar por linea o columna y siempre de forma unidireccional
+    - flex-direction: x; x = ...
+        row         # Fila
+        column      # Columna 
+        x-reverse   # Para row y column empieza de final a principio
+    
+    - El contenido se puede alterar con reverse o con:
+        writing-mode: vertical-lr esto gira los objetos 90 grados
+        direction: rtl esto coloca de derecha a izquierda
+        # Esto es relevante para paginas nacionales, donde el orden de lectura puede variar
+    # Esto tambien se puede cambiar el orden desde los hijos con "orden: 1"
 
+        
 
+    # flex direction como flex wrap puede tocarse a la vez desde "flex-flow"
+"""
+ContenidoFlex=""" 
+    - Desbordamiento
+        Como maneja el contenido y lo organiza de distintos modos 
+            flex-wrap: x; x = ...
+                nowrap      # Es el por defecto, fuerza el tamaño de los hijos para que entre
+                wrap        # Altera el tamaño del continente para que entre el contenido
 
+        # flex direction como flex wrap puede tocarse a la vez desde "flex-flow"
+
+    - Modificacion especifica de los elementos desde 3 opciones desde el los hijos 
+        - flex-grow: x; x = ...
+            0           # Puede aumentar su tamaña base(flex-basis) es el valor por defecto
+        - flex-shrink: x; x = ...
+            1           # Puede reducir su tamaña base(flex-basis) es el valor por defecto
+        - flex-basis: x; x = ...
+            auto        # Tiene un tamaño base automatico
+
+    - Pueds modificar los anteriores 3 por:   
+        - flex: auto    # para que se ajuste automaticamente si tocar los otros tres
+        - flex: 1       # enciende grow y shrink y da a una proporcion respecto al padre
+        - flex: 2       # enciende grow y shrink y da el doble de espacio que los que tienen 1 y asi sucesivamente
+
+"""
+Aliniar=""" 
+
+ """
 
 
 # Herramientas 
